22a5d6862a269f3fa4301ef3bb5cb0bcd7badfde
fangxing <fangxing204@gmail.com>
2025-09-21 20:38:37 +0300

    remove unused files

--- source/Options/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
-import Options from './Options';
-
-const container = document.getElementById('options-root');
-const root = createRoot(container);
-root.render(<Options />);
